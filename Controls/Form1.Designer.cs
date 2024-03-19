30cdbde9d3f8424dd646997bcefc4f4496a76806
Alex <xrlogic1@gmail.com>
2024-03-19 21:19:08 +0400

    *

---
+++ Controls/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickButton = new System.Windows.Forms.Button();
+            this.myTextBox1 = new System.Windows.Forms.TextBox();
+            this.myTextBox2 = new System.Windows.Forms.TextBox();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clickButton
+            // 
+            this.clickButton.Location = new System.Drawing.Point(223, 266);
+            this.clickButton.Name = "clickButton";
+            this.clickButton.Size = new System.Drawing.Size(309, 115);
+            this.clickButton.TabIndex = 0;
+            this.clickButton.Text = "Calculate Sum";
+            this.clickButton.UseVisualStyleBackColor = true;
+            this.clickButton.Click += new System.EventHandler(this.clickButton_Click);
+            // 
+            // myTextBox1
+            // 
+            this.myTextBox1.Location = new System.Drawing.Point(223, 127);
+            this.myTextBox1.Name = "myTextBox1";
+            this.myTextBox1.Size = new System.Drawing.Size(309, 22);
+            this.myTextBox1.TabIndex = 1;
+            // 
+            // myTextBox2
+            // 
+            this.myTextBox2.Location = new System.Drawing.Point(223, 155);
+            this.myTextBox2.Name = "myTextBox2";
+            this.myTextBox2.Size = new System.Drawing.Size(309, 22);
+            this.myTextBox2.TabIndex = 2;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(223, 183);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(309, 22);
+            this.resultTextBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(303, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(437, 91);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 553);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.myTextBox2);
+            this.Controls.Add(this.myTextBox1);
+            this.Controls.Add(this.clickButton);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "MyTitle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button4;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.TextBox resultTextBox;
+
+        private System.Windows.Forms.TextBox myTextBox2;
+
+        private System.Windows.Forms.TextBox myTextBox1;
+
+        private System.Windows.Forms.Button clickButton;
+
+        #endregion
+    }
+}
---
+++ Controls/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Controls;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        MessageBox.Show(@"Form1 Loaded");
+
+        resultTextBox.ReadOnly = true;
+    }
+
+    private void clickButton_Click(object sender, EventArgs e)
+    {
+        resultTextBox.Text = (int.Parse(myTextBox1.Text) + int.Parse(myTextBox2.Text)).ToString();
+    }
+
+    private void button_Click(object sender, EventArgs e)
+    {
+        if (sender is Button button)
+        {
+            switch (button.Text)
+            {
+                case "button1":
+                {
+                    MessageBox.Show(@"It's button1");
+                    break;
+                }
+                case "button2":
+                {
+                    MessageBox.Show(@"It's button2");
+                    break;
+                }
+            }
+        }
+    }
+}
